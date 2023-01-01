e6e6cc2c36b2a622d7267397f6c5005226a5d9ec
michael moreno <michaelmoreno@MacBook-Pro-7.local>
2023-01-01 22:46:19 +0100

    data fetching for basic country profile info

--- script.js
+++ script.js
@@ -8,8 +8,13 @@ function lightMode() {
   element.className = "light-mode";
 }
 
-function getCountryApi() {
-  var requestUrl = "https://restcountries.com/v3.1/name/peru";
+// var card = document.createElement("div");
+
+// var countryName = document.createElement("p")
+// countryName.textContent =
+
+function getAllCountries() {
+  var requestUrl = "https://restcountries.com/v3.1/all";
 
   fetch(requestUrl)
     .then(function (response) {
@@ -17,7 +22,33 @@ function getCountryApi() {
     })
     .then(function (data) {
       console.log(data);
+      for (let i = 0; i < data.length; i++) {
+        var name = data[i].name.common;
+        var continent = data[i].continents;
+        var currency = data[i].currencies;
+        var demonym = data[i].demonyms;
+        var language = data[i].languages;
+        var population = data[i].population;
+        console.log(name);
+        console.log(population);
+        // var countryCapital = data[i].capital[0];
+        // console.log(countryCapital);
+      }
     });
 }
 
-getCountryApi();
+getAllCountries();
+
+// function getACountry() {
+//   var requestUrl = "https://restcountries.com/v3.1/name/brasil";
+
+//   fetch(requestUrl)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//     });
+// }
+
+// getACountry();
