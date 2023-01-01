873692541ea0ae72631d7475bfb5a025cef54ce2
michael moreno <michaelmoreno@MacBook-Pro-7.local>
2023-01-01 21:34:01 +0100

    light mode button added and spacing between light and dark mode fixed

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ function darkMode() {
   element.className = "dark-mode";
 }
 
+function lightMode() {
+  var element = document.body;
+  element.className = "light-mode";
+}
+
 function getCountryApi() {
   var requestUrl = "https://restcountries.com/v3.1/name/peru";
 
